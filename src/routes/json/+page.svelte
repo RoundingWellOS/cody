1517629bd8543e2052283e8f9a9a504369a12132
Zachary Fixler <69738608+zfixler@users.noreply.github.com>
2024-02-26 08:45:41 -0500

    Move Monaco editor into single component

--- src/routes/json/+page.svelte
+++ src/routes/json/+page.svelte
@@ -1,53 +1,6 @@
 <script>
-	import { onMount } from 'svelte';
-	import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-	import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-	import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-	import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-	import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
-
-	export let data;
-	/**@type {HTMLElement}*/
-	let container;
-
-	onMount(async () => {
-		self.MonacoEnvironment = {
-			getWorker: function (_moduleId, label) {
-				if (label === 'json') {
-					return new jsonWorker();
-				}
-				if (label === 'css' || label === 'scss' || label === 'less') {
-					return new cssWorker();
-				}
-				if (label === 'html' || label === 'handlebars' || label === 'razor') {
-					return new htmlWorker();
-				}
-				if (label === 'typescript' || label === 'javascript') {
-					return new tsWorker();
-				}
-				return new editorWorker();
-			}
-		};
-
-		const monaco = await import('monaco-editor');
-		monaco.editor.create(container, {
-			value: data.data,
-			language: 'json',
-			automaticLayout: true,
-			readOnly: true
-		});
-	});
+    import MonacoEditor from '$lib/components/MonacoEditor.svelte';
+    export let data;
 </script>
 
-<h1 class="title">JSON Resource</h1>
-<div class="container" bind:this={container}></div>
-
-<style>
-	.container {
-		height: min(600px, 80vh);
-	}
-
-	.title {
-		text-align: center;
-	}
-</style>
+<MonacoEditor data={data.data} title="JSON Resource" language="json" />
