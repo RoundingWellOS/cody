a6934a1ac0eb33b8bc1dfa751649e5b833ce1837
Zachary Fixler <69738608+zfixler@users.noreply.github.com>
2025-09-16 14:58:23 -0400

    Include year in file naming

--- src/lib/generator/index.js
+++ src/lib/generator/index.js
@@ -2,10 +2,10 @@ import { resolve, join } from 'path';
 
 const INPUT_DIR = 'src/files/input/';
 const OUTPUT_DIR = 'src/files/output/';
-const icdDescriptionsFilePath = resolve(join(INPUT_DIR, 'icd_descriptions.txt'));
-const hccMappingsFilePath = resolve(join(INPUT_DIR, 'hcc_mappings.csv'));
-const icdCodesJsonFilePath = resolve(join(OUTPUT_DIR, 'icd_codes.json'));
-const icdCodesCsvFilePath = resolve(join(OUTPUT_DIR, 'icd_codes.csv'));
+const icdDescriptionsFilePath = (/** @type number */ year) => resolve(join(INPUT_DIR, `${ year }_icd_descriptions.txt`));
+const hccMappingsFilePath = (/** @type number */ year) => resolve(join(INPUT_DIR, `${ year }_hcc_mappings.csv`));
+const icdCodesJsonFilePath = (/** @type number */ year) => resolve(join(OUTPUT_DIR, `${ year }_icd_codes.json`));
+const icdCodesCsvFilePath = (/** @type number */ year) => resolve(join(OUTPUT_DIR, `${ year }_icd_codes.csv`));
 
 export {
     INPUT_DIR,
